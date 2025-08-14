fe9b00fd529e66a92c9dd95834dbd9b08eb2284f
ssszcmawo <belan2470@gmail.com>
2025-08-14 23:05:17 +0200

    Updated main

--- main.cpp
+++ main.cpp
@@ -7,23 +7,41 @@ int main(){
 
     Movie m1("Inception",2010,Genre::Thriller);
     Movie m2("Titanic",1997,Genre::Romance);
+    Movie m3("The Shining", 1980, Genre::Horror);
+    Movie m4("Die Hard", 1988, Genre::Action);
+    Movie m5("The Godfather", 1972, Genre::Drama);
+    Movie m6("Se7en", 1995, Genre::Thriller);
 
-    database.addMovie(m1);
-    database.addMovie(m2);
+    std::vector<Movie> allMovies = {m1,m2,m3,m4,m5};
 
-    database.addRating(m1,5);
-    database.addRating(m2,4);
-    database.addRating(m1,3);
+    for(const auto& movie: allMovies){
+        database.addMovie(movie);
+    }
 
-    auto ratingsInception = database.getRatings(m1);
-    std::cout << "Film ratings " << m1.getTitle() << ": ";
-    for(int r: ratingsInception) std::cout << r << " ";
-    std::cout << "\n";
+    database.addRating(m1, 5);
+    database.addRating(m1, 4);
 
-    auto ratingsTitanic = database.getRatings(m2);
-    std::cout << "Film ratings " << m2.getTitle() << ": ";
-    for(int r: ratingsTitanic) std::cout << r << " ";
-    std::cout << "\n";
+    database.addRating(m2, 5);
+    database.addRating(m2, 5);
+
+    database.addRating(m3, 4);
+    database.addRating(m3, 4);
+
+    database.addRating(m4, 5);
+    database.addRating(m4, 4);
+
+    database.addRating(m5, 5);
+    database.addRating(m5, 3);
+
+    Movie target = m1;
+
+    std::vector<Movie> recs = database.recommendSimilar(target,allMovies,3);
+
+    std::cout << "recommendations for: " << target.getTitle() << "\n";
+
+    for(auto& rec : recs){
+        std::cout << "- " << rec.getTitle() << "\n";
+    }
 
     return 0;
 }
